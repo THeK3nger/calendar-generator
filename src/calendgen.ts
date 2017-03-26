028ad7d2e15101b13f63fc708e24442c95e0255f
Davide Aversa <thek3nger@gmail.com>
2017-03-26 16:12:14 +0200

    Calendar example is now in table form

--- src/calendgen.ts
+++ src/calendgen.ts
@@ -85,14 +85,59 @@ function generateCalendarFromPeriod(planet_period: number, moon_periods: Array<n
     return { description: calendar_description, calendar_parameters: output_parameters };
 }
 
+function generateMonthTableHeader(month_table: JQuery, days_per_week: number, day_names: String[]) {
+    let month_table_header = "";
+    for (let d = 0; d < days_per_week; d++) {
+        month_table_header += `<td>${day_names[d]}</td>`;
+    }
+    month_table.append(`<tr>${month_table_header}</tr>`);
+}
+
+function generateMonthTableContents(month_table: JQuery, starting_week_day: number, days_per_week, days_per_month: number[], current_month: number): number {
+        let week_d = starting_week_day;
+        let m = current_month;
+        let table_day_index = -(week_d % days_per_week); // This is used for aligning the first day to the current week day.
+        let row_day_split = 0;
+        let month_week_line = ""
+        // Fill the empty cells at the begining of the month.
+        // This depends on the starting week day for the current month.
+        while (table_day_index < 0) {
+            month_week_line += "<td></td>";
+            row_day_split++;
+            table_day_index++;
+        }
+        // Now fill the actual table.
+        for (let dm = 0; dm < days_per_month[m]; dm++) {
+            month_week_line += `<td>${dm + 1}</td>`;
+            table_day_index++;
+            week_d++;
+            row_day_split++;
+            if (row_day_split >= days_per_week) {
+                row_day_split = row_day_split - days_per_week;
+                month_table.append(`<tr>${month_week_line}</tr>`);
+                month_week_line = "";
+            }
+        }
+        // Fill the remaining cells in the last row (if any).
+        while (row_day_split != 0 && row_day_split < days_per_week) {
+            month_week_line += "<td></td>";
+            row_day_split++;
+            table_day_index++;
+        }
+        if (month_week_line != "") {
+            month_table.append(`<tr>${month_week_line}</tr>`);
+        }
+        return week_d % days_per_week;
+}
+
 function instantiateCalendar(calendar_parameter: CalendarParameters, days_per_week: number) {
     const year_days = calendar_parameter.days_per_year;
     const months = calendar_parameter.months_per_year;
     const month_base_days = calendar_parameter.base_days_per_month;
 
-    let day_names = ["A", "B", "C", "D", "E", "F", "G"]
+    let day_names = ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"];
 
-    // For now names of days, months and other are just described with D1, M2, and so on...
+    // Allocate all the spare days into random months.
     let days_remainder = year_days - months * month_base_days;
     let days_per_month = [];
     for (let i = 0; i < months; i++) {
@@ -106,14 +151,16 @@ function instantiateCalendar(calendar_parameter: CalendarParameters, days_per_we
 
     let week_d = 0
     for (let m = 0; m < months; m++) {
-        let month_string = "";
-        for (let d = 0; d < days_per_month[m]; d++) {
-            month_string += `${day_names[(week_d % days_per_week)]} ${d + 1} `;
-            week_d++;
-        }
-        let month_p = $("#calendar-example").append(`<p>M${m}: ${month_string}</p>`);
-    }
+        let month_div = $(`<div class="month"></div>`);
+        month_div.append(`<h4>Month ${m + 1}</h4>`)
+        let month_table = $(`<table border="1"></table>`);
+        generateMonthTableHeader(month_table, days_per_week, day_names);
 
+        week_d = generateMonthTableContents(month_table, week_d, days_per_week, days_per_month, m)
+
+        month_table.appendTo(month_div);
+        month_div.appendTo("#calendar-example");
+    }
 }
 
 function continuedFractions(num: number, order: number): Array<number> {
