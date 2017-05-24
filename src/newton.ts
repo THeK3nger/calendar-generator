5736482dfc03d6b1f2ed5283508d294558a02095
Davide Aversa <thek3nger@gmail.com>
2017-05-24 10:04:48 +0200

    Fix bugs in the newton numeric solver

--- src/newton.ts
+++ src/newton.ts
@@ -7,9 +7,10 @@ export function NewtonRoot(f: (x: number)=>number, df: (x: number)=>number, gues
     let ftest = f(x);
     let count = 0;
     // Perform at most 10000 iterations.
-    while (ftest > threshold && count < 10000) {
+    while (Math.abs(ftest) > threshold && count < 10000) {
         x = x - ftest/df(x);
         ftest = f(x);
+        count++;
     }
     return x;
 }
