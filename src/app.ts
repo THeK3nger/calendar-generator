591edace7970e0902ac1082976fad006ee62a0a7
Davide Aversa <thek3nger@gmail.com>
2017-08-15 09:30:31 +0200

    Add logging system

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
+import * as log from 'loglevel'
+
 import * as GeneratorUI from "./interface"
 
+const VERSION = "v0.2.5";
+
 export function init() {
-    console.log("I ADD CLICK");
+    log.setLevel("debug");
+    console.log(`Calendar Generator ${VERSION}`);
+    console.log(`-------------------------`);
+    log.info("Initializing Calendar Generator UI");
     GeneratorUI.initializeComponents();
     // $("#generateButton").click(() => handleForm(viz));
 }
